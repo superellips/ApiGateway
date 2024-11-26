004268a848412417cdac694889236cd51d206902
Erika Bladh-Öström <kiwi.ostrom@gmail.com>
2024-11-26 14:15:07 +0100

    Adds functions for messages.

--- main.go
+++ main.go
@@ -9,21 +9,43 @@ import (
 
 func GetMessages(c *gin.Context) {
 	id := c.Param("gbId")
-
-	response, err := http.Get("guestbookservice/api/version/guestbook/" + id + "/messages")
+	url := "http://guestbookservice:8080/api/version/guestbook/" + id + "/messages"
+	response, err := http.Get(url)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
 		return
 	}
 	defer response.Body.Close()
 	json, err := io.ReadAll(response.Body)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
 		return
 	}
-	c.IndentedJSON(http.StatusOK, json)
+	c.Data(response.StatusCode, "application/json", json)
+}
+
+func PostMessage(c *gin.Context) {
+	id := c.Param("gbId")
+	url := "http://guestbookservice:8080/api/version/guestbook/" + id + "/message"
+	response, err := http.Post(url, "application/json", c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to access service."})
+		return
+	}
+	defer response.Body.Close()
+	json, err := io.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
+		return
+	}
+	c.Data(response.StatusCode, "application/json", json)
 }
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/messages/:gbId")
+	router.GET("/messages/:gbId", GetMessages)
+	router.POST("/messages/:gbId", PostMessage)
+
+	router.Run()
 }
