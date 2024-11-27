be3905262d5adb9c4fd6b057a1fb6646efe190a5
Erika Bladh-Öström <kiwi.ostrom@gmail.com>
2024-11-27 18:35:55 +0100

    Adds user functions.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -53,6 +55,38 @@ func PostMessage(c *gin.Context) {
 	c.Data(response.StatusCode, "application/json", json)
 }
 
+func PostRegisterUser(c *gin.Context) {
+	url := "http://userservice:8080/api/version/users"
+	response, err := http.Post(url, "application/json", c.Request.Body)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	json, err := io.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	c.Data(response.StatusCode, "application/json", json)
+}
+
+func GetUserById(c *gin.Context) {
+	id := c.Param("id")
+	url := "http://userservice:8080/api/version/user/" + id
+	response, err := http.Get(url)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+		return
+	}
+	defer response.Body.Close()
+	json, err := io.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
+		return
+	}
+	c.Data(response.StatusCode, "application/json", json)
+
+}
+
 func IsValidOrigin(c *gin.Context, id string) bool {
 	origin := c.GetHeader("Origin")
 	if origin == "" {
@@ -94,20 +128,41 @@ func GetGuestbookAllowedDomain(id string) (string, error) {
 }
 
 func main() {
+	hostname := "http://" + os.Getenv("GUESTBOOK_ROOT_DOMAIN")
+
 	router := gin.Default()
 
-	defaultCors := cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+	relaxedCors := cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:    []string{"Content-Type", "Origin"},
+		MaxAge:          12 * time.Hour,
 	})
 
-	router.Use(defaultCors)
+	strictCors := cors.New(cors.Config{
+		AllowOrigins:     []string{hostname},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type", "Origin"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
 
-	router.GET("/messages/:gbId", GetMessages)
-	router.POST("/messages/:gbId", PostMessage)
+	public := router.Group("/public")
+	public.Use(relaxedCors)
+	{
+		public.GET("/messages/:gbId", GetMessages)
+		public.POST("/messages/:gbId", PostMessage)
+	}
+
+	private := router.Group("/")
+	private.Use(strictCors)
+	{
+		private.GET("/messages/:gbId", GetMessages)
+		private.POST("/messages/:gbId", PostMessage)
+
+		private.POST("/user/register", PostRegisterUser)
+		private.GET("/user/:id", GetUserById)
+	}
 
 	router.Run()
 }
